73bd7a5259b1f4c19eed0f17ad6f099fafe5945e
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2022-10-03 16:54:42 +0300

    Add library

--- js/index.js
+++ js/index.js
@@ -1,42 +1,4 @@
-const colorPalette = document.querySelector(".color-palette");
-const output = document.querySelector(".output");
 
-colorPalette.addEventListener("click", selectColor);
 
-// This is where delegation «magic» happens
-function selectColor(event) {
-  if (event.target.nodeName !== "BUTTON") {
-    return;
-  }
-
-  const selectedColor = event.target.dataset.color;
-  output.textContent = `Selected color: ${selectedColor}`;
-  output.style.color = selectedColor;
-}
-
-// Some helper functions to render palette items
-createPaletteItems();
-
-function createPaletteItems() {
-  const items = [];
-  for (let i = 0; i < 60; i++) {if (window.CP.shouldStopExecution(0)) break;
-    const color = getRangomColor();
-    const item = document.createElement("button");
-    item.type = "button";
-    item.dataset.color = color;
-    item.style.backgroundColor = color;
-    item.classList.add("item");
-    items.push(item);
-  }window.CP.exitedLoop(0);
-  colorPalette.append(...items);
-}
-
-function getRangomColor() {
-  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
-}
-
-function getRandomHex() {
-  return Math.round(Math.random() * 256).
-  toString(16).
-  padStart(2, "0");
-}
+const result = _.add(2, 3);
+console.log(result); // 5
